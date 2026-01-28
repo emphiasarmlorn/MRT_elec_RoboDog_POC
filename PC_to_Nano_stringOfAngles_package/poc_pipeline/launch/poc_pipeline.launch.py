acc3bcbd0af12a89a2ee77805a297294c9e5db2e
emphiasarmlorn <77542294+emphiasarmlorn@users.noreply.github.com>
2026-01-28 23:04:56 +0530

    Add launch description for poc_pipeline nodes

--- PC_to_Nano_stringOfAngles_package/poc_pipeline/launch/poc_pipeline.launch.py
+++ PC_to_Nano_stringOfAngles_package/poc_pipeline/launch/poc_pipeline.launch.py
@@ -1,1 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='poc_pipeline',
+            executable='dog_command_node',
+            name='dog_command_node',
+            output='screen'
+        ),
+
+        Node(
+            package='poc_pipeline',
+            executable='serial_bridge_node',
+            name='serial_bridge_node',
+            output='screen'
+        )
+
+    ])
